0d4949a5eb4a9008ca7ab524b1196b53cd44edae
Ian <nir2@illinois.edu>
2023-02-16 14:11:31 +0100

    making "add" tool

--- Lucid_Somnambulist/somn/workflows/add.py
+++ Lucid_Somnambulist/somn/workflows/add.py
@@ -6,6 +6,10 @@ from sys import argv
 import molli as ml
 import argparse
 from pathlib import Path
+from datetime import date
+from openbabel import openbabel as ob
+
+temp_work = r"C:\Users\rineharn\workspace/"
 
 
 def get_mol_from_graph(user_input):
@@ -14,7 +18,9 @@ def get_mol_from_graph(user_input):
 
     This is one option for input, others will need to be made for SMILES, etc
     """
-    col = ml.split_cdxml(user_input, enum=False, fmt="{idx}")
+    if user_input.split(".")[1] != "cdxml":
+        raise Exception("cdxml path not specified - wrong extension, but valid path")
+    col = ml.parsing.split_cdxml(user_input, enum=True, fmt="pr{idx}")
     assert isinstance(col, ml.Collection)
     return col
 
@@ -31,21 +37,78 @@ def get_mol_from_smiles(user_input):
     ...
 
 
+def add_hydrogens(col: ml.Collection):
+    output = []
+    for mol in col:
+        obmol = ob.OBMol()
+        obconv = ob.OBConversion()
+        obconv.SetInAndOutFormats("mol2", "mol2")
+        obconv.ReadString(obmol, mol.to_mol2())
+        obmol.AddHydrogens()
+        newmol = ml.Molecule.from_mol2(obconv.WriteString(obmol), mol.name)
+        output.append(newmol)
+    return ml.Collection(f"{col.name}_hadd", output)
+
+
+def preopt_geom(col: ml.Collection):
+    xtb = ml.XTBDriver("preopt", scratch_dir=temp_work + "scratch/", nprocs=1)
+    opt = ml.Concurrent(col, backup_dir=temp_work + "scratch/", update=2)(xtb.optimize)(
+        method="gfnff"
+    )
+    col_opt = ml.Collection(
+        name="opt-test", molecules=[f for f in opt if isinstance(f, ml.Molecule)]
+    )
+    print(opt)
+    return col_opt
+
+
 if __name__ == "__main__":
     assert len(argv) > 1
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cdxml")
-    parser.add_argument("--smi")
+    parser.add_argument(
+        "fmt", nargs=2, help="Format for the input - cdxml or (eventually) smiles"
+    )
+    # parser.add_argument("--cdxml")
+    # parser.add_argument("--smi")
+    parser.add_argument(
+        "r", help="The type of input - nucleophile (nuc) or electrophile (el)"
+    )
     args = parser.parse_args()
-    if args.smi != None:
+    if args.fmt[0] == "smi":
         raise Exception(
             "SMILES parsing not yet supported; please use fpath option for CDXML"
         )
     # if not isinstance(args.cdxml, str):
     #     raise Exception("Please pass a filepath for a cdxml file")
-    try:
-        p = Path(args.cdxml)
-        assert p.exists()
-    except:
-        raise Exception("Please pass a valid path for a cdxml file - parsing failed")
-    collection = get_mol_from_graph(p)
+    elif args.fmt[0] == "cdxml":
+        try:
+            p = Path(args.fmt[1])
+            assert p.exists()
+        except:
+            raise Exception(
+                "Please pass a valid path for a cdxml file - parsing failed"
+            )
+    else:
+        raise Exception("Please pass cdxml as input (eventually smi will be accepted)")
+    ###
+    ###     Parsing molecular drawings into mol objects using firstgen molli. Then, hydrogens are added.
+    ###     These preoptimized structures will then be optimized using xtb, which generally returns reasonable
+    ###     geometries.
+
+    collection = get_mol_from_graph(
+        args.fmt[1]
+    )  # Temp name generated; this will be corrected later, when they are databased, and the names will be set
+    collection.to_zip(r"C:\Users\rineharn\workspace/cdxml_parse.zip")
+    for mol in collection:
+        with open(rf"C:\Users\rineharn\workspace/{mol.name}.mol2", "w") as g:
+            g.write(mol.to_mol2())
+    col_h = add_hydrogens(collection)
+    ID_ = date.today()
+    col_h.to_zip(temp_work + f"new_{args.r}_input_{ID_}.zip")
+    for mol in col_h:
+        with open(rf"C:\Users\rineharn\workspace/{mol.name}_hadd.mol2", "w") as g:
+            g.write(mol.to_mol2())
+    preopt = preopt_geom(col_h)
+    for mol in preopt:
+        with open(rf"C:\Users\rineharn\workspace/{mol.name}_preopt.mol2", "w") as g:
+            g.write(mol.to_mol2())
